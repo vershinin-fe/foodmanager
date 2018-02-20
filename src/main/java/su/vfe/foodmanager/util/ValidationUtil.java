cbcdc7d1cd829a3a72726a23533bd547b3cb1db2
vershinin-fe <few_w@mail.ru>
2018-02-20 23:17:07 +0500

    Add MessageSource and some messages Add getRootCause method to ValidationUtil Add MessageUtil for getting messages from bundle Implement Exception handling for REST controllers

--- src/main/java/su/vfe/foodmanager/util/ValidationUtil.java
+++ src/main/java/su/vfe/foodmanager/util/ValidationUtil.java
@@ -57,4 +57,14 @@ public class ValidationUtil {
             throw new FamilyMembershipChangeException("id=" + user.getId());
         }
     }
+
+    public static Throwable getRootCause(Throwable t) {
+        Throwable result = t;
+        Throwable cause;
+
+        while (null != (cause = result.getCause()) && (result != cause)) {
+            result = cause;
+        }
+        return result;
+    }
 }
--- src/main/java/su/vfe/foodmanager/web/ExceptionInfoHandler.java
+++ src/main/java/su/vfe/foodmanager/web/ExceptionInfoHandler.java
@@ -1,36 +1,94 @@
 package su.vfe.foodmanager.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import su.vfe.foodmanager.util.exception.NotFoundException;
-
+import su.vfe.foodmanager.util.ValidationUtil;
+import su.vfe.foodmanager.util.exception.ApplicationException;
+import su.vfe.foodmanager.util.exception.ErrorInfo;
+import su.vfe.foodmanager.util.exception.ErrorType;
+import static su.vfe.foodmanager.util.exception.ErrorType.*;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
+import java.util.*;
 
 @RestControllerAdvice(annotations = RestController.class)
 public class ExceptionInfoHandler {
 
-    //TODO: Temporary stub for test NotFoundException
-    @ExceptionHandler(NotFoundException.class)
-    @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
-    public void notFoundError() { }
+    private static Logger log = LoggerFactory.getLogger(ExceptionInfoHandler.class);
 
-    //TODO: Temporary stub for test MethodArgumentNotValidException
-    @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)  // 422
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public void bindValidationError(HttpServletRequest req, MethodArgumentNotValidException e) {
-        BindingResult result = e.getBindingResult();
+    public static final String EXCEPTION_DUPLICATE_EMAIL = "exception.user.duplicateEmail";
 
-        List<FieldError> details = result.getFieldErrors();
+    private static final Map<String, String> CONSTRAINS_I18N_MAP = Collections.unmodifiableMap(
+            new HashMap<String, String>() {
+                {
+                    put("users_unique_email_idx", EXCEPTION_DUPLICATE_EMAIL);
+                    //Maybe add some new constraints later
+                }
+            });
 
-        for (FieldError fe : details) {
-            System.out.println(fe.getField() + "     " + fe.getRejectedValue());
+    @Autowired
+    private MessageUtil messageUtil;
+
+    @ExceptionHandler(ApplicationException.class)
+    public ResponseEntity<ErrorInfo> applicationError(HttpServletRequest req, ApplicationException appEx) {
+        ErrorInfo errorInfo = logAndGetErrorInfo(req, appEx, false, appEx.getType(), messageUtil.getMessage(appEx));
+        return new ResponseEntity<>(errorInfo, appEx.getHttpStatus());
+    }
+
+    @ResponseStatus(value = HttpStatus.CONFLICT)  // 409
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ErrorInfo conflict(HttpServletRequest req, DataIntegrityViolationException e) {
+        String rootMsg = ValidationUtil.getRootCause(e).getMessage();
+        if (rootMsg != null) {
+            String lowerCaseMsg = rootMsg.toLowerCase();
+            Optional<Map.Entry<String, String>> entry = CONSTRAINS_I18N_MAP.entrySet().stream()
+                    .filter(it -> lowerCaseMsg.contains(it.getKey()))
+                    .findAny();
+            if (entry.isPresent()) {
+                return logAndGetErrorInfo(req, e, false, DATA_ERROR, messageUtil.getMessage(entry.get().getValue()));
+            }
         }
+        return logAndGetErrorInfo(req, e, true, DATA_ERROR);
+    }
+
+    @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)  // 422
+    @ExceptionHandler({BindException.class, MethodArgumentNotValidException.class})
+    public ErrorInfo bindValidationError(HttpServletRequest req, Exception e) {
+        BindingResult result = e instanceof BindException ?
+                ((BindException) e).getBindingResult() : ((MethodArgumentNotValidException) e).getBindingResult();
+
+        String[] details = result.getFieldErrors().stream()
+                .map(fe -> messageUtil.getMessage(fe))
+                .toArray(String[]::new);
+
+        return logAndGetErrorInfo(req, e, false, VALIDATION_ERROR, details);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public ErrorInfo handleError(HttpServletRequest req, Exception e) {
+        return logAndGetErrorInfo(req, e, true, APP_ERROR);
+    }
+
+    private ErrorInfo logAndGetErrorInfo(HttpServletRequest req, Exception e, boolean logException, ErrorType errorType, String... details) {
+        Throwable rootCause = ValidationUtil.getRootCause(e);
+        if (logException) {
+            log.error(errorType + " at request " + req.getRequestURL(), rootCause);
+        } else {
+            log.warn("{} at request  {}: {}", errorType, req.getRequestURL(), rootCause.toString());
+        }
+        return new ErrorInfo(req.getRequestURL(), errorType,
+                messageUtil.getMessage(errorType.getErrorCode()),
+                details.length != 0 ? details : new String[]{rootCause.toString()});
     }
 }
